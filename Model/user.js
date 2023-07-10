965c4fe302dac4b1195678d981fc81c568a5282e
hyonie <dev.hyonie@gmail.com>
2023-07-10 18:22:48 +0900

    feat: 회원 탈퇴

--- Model/user.js
+++ Model/user.js
@@ -13,6 +13,13 @@ const userSchema = new mongoose.Schema(
       trim: true,
     },
     email: { type: String, required: true, unique: true, lowercase: true },
+    phone: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    address: { type: String, require: true },
+    detail_address: { type: String },
   },
   { collection: "users" }
 );
--- controller/main.js
+++ controller/main.js
@@ -10,10 +10,6 @@ exports.cart = (req, res) => {
   res.render("cart");
 };
 
-exports.myProfile = (req, res) => {
-  res.render("myProfile");
-};
-
 exports.deleteUser = (req, res) => {
   res.render("deleteUser");
 };
