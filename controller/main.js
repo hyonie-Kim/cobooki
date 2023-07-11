24da3f1e3a1a29c448379300119a125a96bf2b7c
hyonie <dev.hyonie@gmail.com>
2023-07-11 16:08:10 +0900

    feat: 메인 데이타

--- controller/main.js
+++ controller/main.js
@@ -1,15 +1,31 @@
-exports.mainRender = (req, res) => {
-  res.render("index");
+const { Product } = require("../Model/product");
+const { Counter } = require("../Model/counter");
+
+const mainController = {
+  mainRender(req, res) {
+    Product.find()
+      .exec()
+      .then((postData) => {
+        console.log(postData);
+        res.render("index", { postData: postData });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.render("index", { postData: [] });
+      });
+  },
+
+  order(req, res) {
+    res.render("order");
+  },
+
+  cart(req, res) {
+    res.render("cart");
+  },
+
+  deleteUser(req, res) {
+    res.render("deleteUser");
+  },
 };
 
-exports.order = (req, res) => {
-  res.render("order");
-};
-
-exports.cart = (req, res) => {
-  res.render("cart");
-};
-
-exports.deleteUser = (req, res) => {
-  res.render("deleteUser");
-};
+module.exports = mainController;
